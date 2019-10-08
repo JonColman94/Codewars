98c45b92def1fd99435e852072494b551f74acfc
Jonathan Colman <jrc1g12@soton.ac.uk>
2019-10-08 20:54:01 +0100

    Completed the Build a pile of Cubes Kata

---
+++ C#/BuildPileCubes/ASum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BuildPileCubes
+{
+    public class ASum
+    {
+        // Uses the knowledge that sum of r^3 = ((n(n+1))/2)^2
+        public static long findNb(long m)
+        {
+            long sqrt = (long)Math.Sqrt(m);
+            if (sqrt * sqrt != m) return -1;
+            long n = (long)Math.Sqrt(sqrt * 2);
+            return (n * (n + 1) == sqrt * 2) ? n : -1;
+
+        }
+
+    }
+}
