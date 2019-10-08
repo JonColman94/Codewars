26dfb047bb8afffb1fd96455e3b25c8302d38069
Jonathan Colman <jrc1g12@soton.ac.uk>
2019-10-08 21:22:03 +0100

    Completed List Filtering Kata

---
+++ C#/ListFilterer/ListFilterer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class ListFilterer
+{
+
+    // Use LINQ to filter out strings;
+    public static IEnumerable<int> GetIntegersFromList(List<object> listOfItems)
+    {
+        return listOfItems.OfType<int>();
+    }
+}
