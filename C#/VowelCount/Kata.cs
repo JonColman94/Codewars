a167aea430842f0fe260254d3ee5792b6ccc2254
Jonathan Colman <jrc1g12@soton.ac.uk>
2019-10-08 21:46:15 +0100

    Completed Vowel Count Kata

---
+++ C#/VowelCount/Kata.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Kata
+{
+   
+    //If length if 0 or string is null, return 0, else count vowels
+    public static int GetVowelCount(string str)
+    {
+        return (str.Length == 0 || str == null) ? 0 : str.Count(c => "aeiouAEIOU".Contains(c));
+    }
+}
